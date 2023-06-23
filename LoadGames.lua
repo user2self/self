c071f83e31fcc31305c18b73ded202cd57960372
self2user <60581104+userp1@users.noreply.github.com>
2023-06-23 10:28:05 -0600

    Create LoadGames.lua

---
+++ LoadGames.lua
@@ -0,0 +1,4 @@
+if game.PlaceId == 13127800756 then --> Arm Wrestle Simulator
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/userp1/self/main/ArmWrestleSimulator.lua"))()
+end
+--More games coming soon.
