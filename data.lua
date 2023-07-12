19cfe15a99d61725225ff9b8f439050b6ea83550
self2user <60581104+userp1@users.noreply.github.com>
2023-07-12 13:18:53 -0600

    Create data.lua

---
+++ data.lua
@@ -0,0 +1,15 @@
+local data = {}
+data.version = function()
+  return "1.8.5"
+end
+
+data.key = function()
+  return "76OKGHMNGF"
+end
+
+data.info = function()
+  return {
+    title = "ShinyTool Info",
+    description = "Visit our discord server to get key."
+  }
+end
